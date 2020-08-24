e23bce36aa9ac4d0d95a0b4a97fd3e85563a1883
LowRiHao <haroic1997@gmail.com>
2020-08-24 08:58:52 +0800

    Implemented Echo and Exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
+
 public class Duke {
     public static void main(String[] args) {
+        boolean byeDetected=false;
+        Scanner in = new Scanner(System.in);
+        String input;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,11 +14,34 @@ public class Duke {
 
         String welcomeMessage=
                 "________________________________\n"
-                +"Hello! I'm Duke\n"
-                +"What can I do for you?\n"
-                +"________________________________\n"
-                +"Bye. Hope to see you again soon!\n"
+                +"Whats up people! Duke is here to save the day\n"
+                +"Soooo, what can I do for you?\n"
+                +"________________________________\n";
+
+        String byeMessage=
+                "Aye captain. This is Duke Signing out!\n"
                 +"________________________________";
+
         System.out.println(welcomeMessage);
+
+
+        while(!byeDetected){
+            input = in.nextLine();
+            String[] words = input.split(" ");
+            for (String word:words){
+                if (word.equalsIgnoreCase("bye")){
+                    byeDetected=true;
+                }
+            }
+            if(!byeDetected)
+            {
+                System.out.println(input);
+            }
+        }
+        System.out.println(byeMessage);
+
     }
+
+
+
 }
