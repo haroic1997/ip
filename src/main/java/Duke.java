4eca54d2bffa3b12a52d411d89a6d21c66ab23f9
LowRiHao <haroic1997@gmail.com>
2020-08-24 09:55:05 +0800

    Add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         boolean byeDetected=false;
+        boolean listDetected=false;
         Scanner in = new Scanner(System.in);
+        String[] list=new String[100];
+        int listCount=0;
         String input;
+        String border="\n________________________________";
+        String borderWithoutSkip="________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -26,22 +31,40 @@ public class Duke {
 
 
         while(!byeDetected){
+            listDetected=false;
             input = in.nextLine();
             String[] words = input.split(" ");
             for (String word:words){
                 if (word.equalsIgnoreCase("bye")){
                     byeDetected=true;
+                    break;
+                }
+                else if(word.equalsIgnoreCase("list")) {
+                    if (listCount==0)
+                    {
+                        System.out.println("No list Detected, add some text!"+border);
+                    }
+                    else{
+                        for(int i=0;i<listCount;i++){
+                            int position =i+1;
+                            System.out.println(position +". "+ list[i]);
+                        }
+                        System.out.println(borderWithoutSkip);
+                    }
+                    listDetected=true;
+                    break;
                 }
             }
-            if(!byeDetected)
+            if(!byeDetected&&!listDetected)
             {
-                System.out.println(input);
+                list[listCount]=input;
+                listCount++;
+                System.out.println("added: "+ input+border);
             }
+
         }
         System.out.println(byeMessage);
 
     }
-
-
 
 }
