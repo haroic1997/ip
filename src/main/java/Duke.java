b955dda4d3545b5de5d1fe0b8e007a40ff83e4c0
LowRiHao <haroic1997@gmail.com>
2020-09-03 14:18:55 +0800

    remove unneccesary for loop structure

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,9 +7,7 @@ public class Duke {
     static final String BORDER="\n________________________________";
     static final String BORDER_WITHOUT_SKIP="________________________________";
     public static void main(String[] args) {
-        /* To check if keywords like bye, list or done is used */
         boolean byeDetected=false;
-        boolean commandsDetected=false;
         Scanner in = new Scanner(System.in);
         String input;
         String logo = " ____        _        \n"
@@ -32,106 +30,95 @@ public class Duke {
 
 
         while (!byeDetected) {
-            commandsDetected=false;
             input = in.nextLine();
             String[] words = input.split(" ");
-            for (int i=0;i<words.length;i++) {
-                switch (words[i].toLowerCase()) {
-                case "bye": {
-                    byeDetected = true;
-                    break;
-                }
-                case "list": {
-                    if (listCount == 0) {
-                        System.out.println("No list Detected, add some text!" + BORDER);
-                    } else {
-                        for (int j = 0; j < listCount; j++) {
-                            int position = j + 1;
-                            System.out.println(position + "." + list[j].getTaskType() + "["
-                                    + list[j].getStatusIcon() + "] "
-                                    + list[j].getFullDescription());
-                        }
-                        System.out.println(BORDER_WITHOUT_SKIP);
+            switch (words[0].toLowerCase()) {
+            case "bye": {
+                byeDetected = true;
+                break;
+            }
+            case "list": {
+                if (listCount == 0) {
+                    System.out.println("No list Detected, add some text!" + BORDER);
+                } else {
+                    for (int j = 0; j < listCount; j++) {
+                        int position = j + 1;
+                        System.out.println(position + "." + list[j].getTaskType() + "["
+                                + list[j].getStatusIcon() + "] "
+                                + list[j].getFullDescription());
                     }
-                    commandsDetected = true;
-                    break;
+                    System.out.println(BORDER_WITHOUT_SKIP);
                 }
-                case "done": {
+                break;
+            }
+            case "done": {
 
-                    int taskNum = Integer.parseInt(words[i + 1]);
-                    list[taskNum - 1].maskAsDone();
-                    System.out.println("Nice! I've marked this task as done:"
-                            + "\n"
-                            + "["
-                            + list[taskNum - 1].getStatusIcon()
-                            + "] "
-                            + list[taskNum - 1].getFullDescription()
-                            + BORDER
-                    );
-                    commandsDetected = true;
-                    break;
+                int taskNum = Integer.parseInt(words[1]);
+                list[taskNum - 1].maskAsDone();
+                System.out.println("Nice! I've marked this task as done:"
+                        + "\n"
+                        + "["
+                        + list[taskNum - 1].getStatusIcon()
+                        + "] "
+                        + list[taskNum - 1].getFullDescription()
+                        + BORDER
+                );
+                break;
+            }
+            case "todo": {
+                String description = "";
+                for (int j = 1; j < words.length; j++) {
+                    description += words[j] + " ";
                 }
-                case "todo": {
-                    String description = "";
-                    for (int j = 1; j < words.length; j++) {
-                        description += words[j] + " ";
+                ToDo t = new ToDo(description);
+                addToList(t);
+                break;
+            }
+            case "deadline":{
+                int byPosition=0;
+                String byDescription="";
+                String description = "";
+                for(int j =1; j< words.length;j++){
+                    if(words[j].contains("/by")){
+                        byPosition=j;
+                        break;
                     }
-                    ToDo t = new ToDo(description);
-                    addToList(t);
-                    commandsDetected = true;
-                    break;
+                    else{
+                        description+= words[j]+ " ";
+                    }
                 }
-                case "deadline":{
-                    int byPosition=0;
-                    String byDescription="";
-                    String description = "";
-                    for(int j =1; j< words.length;j++){
-                        if(words[j].contains("/by")){
-                            byPosition=j;
-                           break;
-                        }
-                        else{
-                            description+= words[j]+ " ";
-                        }
-                    }
-                    for (int k = byPosition+1; k < words.length; k++) {
-                        byDescription= byDescription +" "+ words[k];
-                    }
-                    Deadline d= new Deadline(description,byDescription);
-                    addToList(d);
-                    commandsDetected = true;
-                    break;
+                for (int k = byPosition+1; k < words.length; k++) {
+                    byDescription= byDescription +" "+ words[k];
                 }
-                case "event":{
-                    int atPosition=0;
-                    String atDescription="";
-                    String description = "";
-                    for(int j =1; j< words.length;j++){
-                        if(words[j].contains("/at")){
-                            atPosition=j;
-                            break;
-                        }
-                        else{
-                            description+= words[j]+ " ";
-                        }
+                Deadline d= new Deadline(description,byDescription);
+                addToList(d);
+                break;
+            }
+            case "event":{
+                int atPosition=0;
+                String atDescription="";
+                String description = "";
+                for(int j =1; j< words.length;j++){
+                    if(words[j].contains("/at")){
+                        atPosition=j;
+                        break;
                     }
-                    for (int k = atPosition+1; k < words.length; k++) {
-                        atDescription= atDescription +" "+ words[k];
+                    else{
+                        description+= words[j]+ " ";
                     }
-                    Event e= new Event(description,atDescription);
-                    addToList(e);
-                    commandsDetected = true;
-                    break;
                 }
+                for (int k = atPosition+1; k < words.length; k++) {
+                    atDescription= atDescription +" "+ words[k];
+                }
+                Event e= new Event(description,atDescription);
+                addToList(e);
+                break;
+            }
 
-                default:{
-                    System.out.println("No proper Commands Detected");
-                    break;
-                }
-                }
-                if(commandsDetected){
-                    break;
-                }
+            default:{
+                System.out.println("No proper Commands Detected");
+                break;
+            }
             }
 
         }
